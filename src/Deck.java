09bea8e21230e9867069b7b28de2ac49b3e156f2
John Junsong Zhang <junsong.zhang@vanderbilt.edu>
2014-07-19 20:23:57 -0700

    created deck class

---
+++ src/Deck.java
@@ -0,0 +1,16 @@
+
+public class Deck {
+	private static int[] cards;
+	
+	public static void init() {
+		cards = new int[52]; 
+	}
+	
+	public static int getCard() {
+		return 0;
+	}
+	
+	public static void shuffle() {
+		
+	}
+}
